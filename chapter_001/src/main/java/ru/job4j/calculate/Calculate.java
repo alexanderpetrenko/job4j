dfe795f30c9891e0b5657269d0be1e5dbae412ce
Alexander Petrenko <Lexer8@gmail.com>
2019-01-19 22:05:22 +0100

    1.3. JavaDoc. Code convention[#106476]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,20 @@
 package ru.job4j.calculate;
 
+/**
+ * Объект класса {@code Calculate} выводит строку
+ * в консоль.
+ *
+ * @author Александр Петренко (Lexer8@gmail.com)
+ * @version 1.0
+ */
 public class Calculate{
 	
+	/**
+	 * Точка входа в программу.
+	 *
+	 * @param args - аргументы из командной строки
+	 * в виде массива объектов String.
+	 */
 	public static void main(String[] args){
 		System.out.println("We will not use 'Hello, World!'");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Пакет для задания Калькулятор
+*
+* @author Александр Петренко (Lexer8@gmail.com)
+* @version $Id$
+* @since 1.0
+*/
+package ru.job4j.calculate;
