b9db210ea9626127cac3838723004a97094154e9
Alexander Petrenko <Lexer8@gmail.com>
2019-01-22 22:57:02 +0100

    4.1. DummyBot - empty lines removing.

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -16,15 +16,12 @@ public class DummyBot {
      * @return Answer.
      */
     public String answer(String question) {
-
         String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
-
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
         } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
-
         return rsl;
     }
 }
