58e1ee6ffe5d4c29fc605567f0adc98782edb0c2
Alexander Petrenko <Lexer8@gmail.com>
2019-01-22 17:10:54 +0100

    3.4. Расстояние между точками в системе координат[#106468]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,64 @@
+package ru.job4j.condition;
+
+/**
+ * Distance between two points
+ * in the coordinate system.
+ *
+ * @author Alexander Petrenko (Lexer8@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
+public class Point {
+
+    /**
+     * X coordinate.
+     */
+    private int x;
+
+    /**
+     * Y coordinate.
+     */
+    private int y;
+
+    /**
+     * Point class constructor.
+     * @param x X coordinate.
+     * @param y Y coordinate.
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Distance calculation method.
+     * @param that End point.
+     * @return Distance between starting and ending points.
+     */
+    public double distanceTo(Point that) {
+        return Math.sqrt(
+                Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2)
+        );
+    }
+
+    /**
+     * Points initialization and method calling.
+     * @param args Command line arguments as an array
+     *             of String objects.
+     */
+    public static void main(String[] args) {
+        // Points initialization.
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+
+        // Initial data output.
+        System.out.println("x1 = " + a.x);
+        System.out.println("y1 = " + a.y);
+        System.out.println("x2 = " + b.x);
+        System.out.println("y2 = " + b.y);
+
+        // Method call.
+        double result = a.distanceTo(b);
+        System.out.println("Distance = " + result);
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package is used for Condition tasks.
+ *
+ * @author Alexander Petrenko (Lexer8@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
+package ru.job4j.condition;
