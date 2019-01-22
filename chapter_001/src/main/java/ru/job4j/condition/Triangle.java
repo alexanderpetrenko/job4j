b1c575baf9688bbff148d6446b905a3fd1f5a2ec
Alexander Petrenko <Lexer8@gmail.com>
2019-01-22 23:15:18 +0100

    4.3. Triangle area.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double period(double ab, double ac, double bc) {
+        return ab + ac + bc;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = this.a.distanceTo(b);
+        double ac = this.a.distanceTo(c);
+        double bc = this.b.distanceTo(c);
+        double p = period(ab,ac,bc);
+        return rsl;
+    }
+
+    private boolean exist(double ab, double ac, double bc) {
+        return false;
+    }
+}
