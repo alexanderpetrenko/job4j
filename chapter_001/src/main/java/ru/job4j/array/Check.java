e47e1c9c7f8778d3cdcf73f15727f202d5ede73b
Alexander Petrenko <Lexer8@gmail.com>
2019-01-24 17:28:28 +0100

    6.3. Массив заполнен true или false[#106457]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,11 +1,30 @@
 package ru.job4j.array;
 
+/**
+ * An object of the {@code Check} class is used for
+ * check the combination of array elements.
+ *
+ * @author Alexander Petrenko (Lexer8@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Check {
+    /**
+     * This method checks if all array elements
+     * are the same.
+     *
+     * @param data Data array with boolean elements.
+     * @return Result is {@code true},
+     * if all elements in an array are the same.
+     */
     public boolean mono(boolean[] data) {
-        boolean result = false;
-//        for () {
-//
-//        }
+        boolean result = true;
+        for (int i = 1; i <= data.length - 1; i++) {
+            if (data[i - 1] != data[i]) {
+                result = false;
+                break;
+            }
+        }
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/CheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -5,27 +5,43 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Array check test.
+ *
+ * @author Alexander Petrenko (Lexer8@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CheckTest {
+    /**
+     * All array elements are true.
+     */
     @Test
-    public void whenDataMonoByTrueThenTrue(){
+    public void whenDataMonoByTrueThenTrue() {
         Check check = new Check();
-        boolean[] input = new boolean[] {true, true, true};
+        boolean[] input = new boolean[]{true, true, true};
         boolean result = check.mono(input);
         assertThat(result, is(true));
     }
 
+    /**
+     * Mixed array elements: true and false.
+     */
     @Test
-    public void whenDataNotMonoByTrueThenFalse(){
+    public void whenDataNotMonoByTrueThenFalse() {
         Check check = new Check();
-        boolean[] input = new boolean[] {true, false, true};
+        boolean[] input = new boolean[]{true, false, true};
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
 
+    /**
+     * All array elements are false.
+     */
     @Test
-    public void whenDataMonoByFalseThenTrue(){
+    public void whenDataMonoByFalseThenFalse() {
         Check check = new Check();
-        boolean[] input = new boolean[] {false, false, false};
+        boolean[] input = new boolean[]{false, false, false};
         boolean result = check.mono(input);
         assertThat(result, is(true));
     }
