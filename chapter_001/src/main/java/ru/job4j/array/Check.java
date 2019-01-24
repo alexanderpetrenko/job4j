a13bbe9f755d535e262bb86e72be5ff95ed63615
Alexander Petrenko <Lexer8@gmail.com>
2019-01-24 14:47:58 +0100

    6.3. Массив true/false [#106457]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class Check {
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+//        for () {
+//
+//        }
+        return result;
+    }
+}
