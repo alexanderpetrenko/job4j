a1310bc16c1b058bdd268d6a28a2801946be51d9
Alexander Petrenko <Lexer8@gmail.com>
2019-02-08 13:11:11 +0100

    Tracker class [#106532]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker;
+
+public class Item {
+    private String id;
+    private String name;
+    private String desc;
+    private long created;
+    private String[] comments;
+
+    public Item(String name, String desc, long created) {
+        this.name = name;
+        this.desc = desc;
+        this.created = created;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    //    public static void main(String[] args) {
+//        System.out.println();
+//    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,66 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+
+public class Tracker {
+    /**
+     * List of items.
+     */
+    private final Item[] items = new Item[100];
+    /**
+     * Next item index.
+     */
+    private int position = 0;
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(id)) {
+                this.items[i] = item;
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public boolean delete(String id) {
+        boolean result = false;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(id)) {
+                System.arraycopy(this.items, i + 1, this.items, i, this.position - 1 - i);
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Item[] findAll() {
+        return this.items;
+    }
+
+//    public Item[] findByName(String key){
+//        Item[] result;
+//    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(id)) {
+                result = this.items[i];
+                break;
+            }
+        }
+        return result;
+    }
+
+    public String generateId() {
+        return LocalDateTime.now().toString() + Math.random();
+    }
+}
