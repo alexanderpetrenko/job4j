d700e7324f1d682acba4e8bcbaa009a3ca101fa4
Alexander Petrenko <Lexer8@gmail.com>
2019-03-16 15:04:15 +0100

    Кофемашина - задание выполено.

--- chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
@@ -1,23 +1,77 @@
 package ru.job4j.coffee;
 
-public class CoffeeMachine {
+import java.util.Arrays;
 
+/**
+ * The {@code CoffeeMachine} class calculates change for the coffee.
+ *
+ * @author Alexander Petrenko (Lexer8@gmail.com)
+ * @version 1.0
+ * @since 16.03.2019
+ */
+public class CoffeeMachine {
+    /**
+     * Array of coins for change.
+     */
     private int[] coins;
 
+    /**
+     * The class constructor initialises array of coins for change.
+     *
+     * @param coins Array of coins, that are available for change.
+     */
     public CoffeeMachine(int[] coins) {
-        this.coins = coins;
-//        Arrays.sort(this.coins);
+        this.coins = CoffeeMachine.descSort(coins);
     }
 
+    /**
+     * The method for descending sort of the array of coins.
+     * The method uses Bubble Sort algorithm.
+     *
+     * @param array The array of coins for change.
+     * @return The array of coins sorted in descending order.
+     */
+    private static int[] descSort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+
+    /**
+     * The method counts the customer's change for coffee.
+     * @param value The customers money for the coffee.
+     * @param price The coffee price.
+     * @return Array of coins for change.
+     * @throws NotEnoughMoney - exception, when customer has not enough money for the coffee.
+     */
     public int[] changes(int value, int price) {
         int delta = value - price;
         if (delta < 0) {
             throw new NotEnoughMoney("You have not enough money for your order.");
         }
-        int[] rst = new int[delta];
-//        for (int coin : coins) {
-//            delta
-//        }
-        return rst;
+        int[] result = new int[delta];
+        int position = 0;
+        if (delta == 0) {
+            position++;
+        } else {
+            for (int coin : coins) {
+                while (delta != 0) {
+                    if (delta - coin >= 0) {
+                        delta -= coin;
+                        result[position++] = coin;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return Arrays.copyOf(result, position);
     }
 }
--- chapter_002/src/test/java/ru/job4j/coffee/CoffeeMachineTest.java
+++ chapter_002/src/test/java/ru/job4j/coffee/CoffeeMachineTest.java
@@ -1,4 +1,47 @@
 package ru.job4j.coffee;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for {@code CoffeeMachine} class.
+ *
+ * @author Alexander Petrenko (Lexer8@gmail.com)
+ * @version 1.0
+ * @since 16.03.2019
+ */
 public class CoffeeMachineTest {
+    /**
+     * Test of the situation, when a customer needs change.
+     */
+    @Test
+    public void whenCustomerHasEnoughMoney() {
+        int[] coins = new int[]{1, 2, 5, 10};
+        CoffeeMachine machine = new CoffeeMachine(coins);
+        int[] expected = new int[]{10, 5};
+        assertThat(machine.changes(50, 35), is(expected));
+    }
+
+    /**
+     * Test of the situation, when a customer doesn't need change.
+     */
+    @Test
+    public void whenCustomerNeedNotChange() {
+        int[] coins = new int[]{1, 2, 5, 10};
+        CoffeeMachine machine = new CoffeeMachine(coins);
+        int[] expected = new int[]{0};
+        assertThat(machine.changes(50, 50), is(expected));
+    }
+
+    /**
+     * Test of the situation, when a customer hasn't got enough money.
+     */
+    @Test(expected = NotEnoughMoney.class)
+    public void whenCustomerHasNotEnoughMoney() {
+        int[] coins = new int[]{1, 2, 5, 10};
+        CoffeeMachine machine = new CoffeeMachine(coins);
+        machine.changes(35, 50);
+    }
 }
