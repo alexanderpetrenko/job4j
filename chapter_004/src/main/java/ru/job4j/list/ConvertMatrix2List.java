4df820ceb028b80780bafe73e7cefe10a18ed0a5
Alexander Petrenko <Lexer8@gmail.com>
2019-04-05 09:17:06 +0200

    Convert Matrix to List on Stream API.

---
+++ chapter_004/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,21 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * The {@code ConvertMatrix2List} class provides converting operation
+ * from a two-dimensional array to a List.
+ *
+ * @author Alexander Petrenko (Lexer8@gmail.com)
+ * @version 1.0
+ * @since 1.0
+ */
+public class ConvertMatrix2List {
+    public List<Integer> toList(int[][] array) {
+        return Arrays.stream(array)
+                .flatMapToInt(Arrays::stream).boxed()
+                .collect(Collectors.toList());
+    }
+}
