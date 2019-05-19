8681949f36eb32223c4f967ff856bbb3e8f40b7a
Alexander Petrenko <Lexer8@gmail.com>
2019-05-19 17:12:26 +0200

    Поиск ближайшего общего предка двух вершин бинарного дерева - алгоритм сведен в один метод findParent.

--- chapter_004/src/main/java/ru/job4j/binarytree/BinaryTree.java
+++ chapter_004/src/main/java/ru/job4j/binarytree/BinaryTree.java
@@ -1,9 +1,5 @@
 package ru.job4j.binarytree;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
 /**
  * The {@code BinaryTree} class provides searching operation in the binary tree.
  *
@@ -13,60 +9,48 @@ import java.util.List;
  */
 public class BinaryTree {
     /**
-     * List of Nodes from the First Node to the Root of the Binary Tree.
-     */
-    private List<Node> branch1 = new ArrayList<>();
-    /**
-     * List of Nodes from the Second Node to the Root of the Binary Tree.
-     */
-    private List<Node> branch2 = new ArrayList<>();
-
-    /**
-     * Constructor initialize two branches from each node to the Root
-     * of the Binary tree.
+     * Searches the Least Common Node of two binary tree nodes.
      *
-     * @param node1 Value of the First Node (1< = node1 <= 10^9).
-     * @param node2 Value of the Second Node (1< = node2 <= 10^9).
+     * @param left  The Value of a left node (1 <= node1 <= 10^9).
+     * @param right The Value of a right node (1 <= node1 <= 10^9).
+     * @return The value of the Least Common Node.
      */
-    public BinaryTree(int node1, int node2) {
-        setBranch(this.branch1, new Node(node1));
-        setBranch(this.branch2, new Node(node2));
-    }
-
-    /**
-     * The method provides parent node search and adds it to the list.
-     *
-     * @param branch List of Nodes from input node to the Root.
-     * @param node   Input Node.
-     */
-    private void setBranch(List<Node> branch, Node node) {
-        branch.add(node);
-        if (node.getValue() >= 1) {
-            Node parentNode;
-            if (node.getValue() % 2 == 0) {
-                parentNode = new Node(node.getValue() / 2);
+    public int findParent(int left, int right) {
+        int hLeft = depth(left);
+        int hRight = depth(right);
+        while (hLeft != hRight) {
+            if (hLeft > hRight) {
+                left = parent(left);
+                hLeft--;
             } else {
-                parentNode = new Node((node.getValue() - 1) / 2);
+                right = parent(right);
+                hRight--;
             }
-            this.setBranch(branch, parentNode);
         }
+        while (left != right) {
+            left = parent(left);
+            right = parent(right);
+        }
+        return left;
     }
 
     /**
-     * The method searches the Least Common Node of two branches.
+     * Calculates the depth level of the binary tree node.
      *
-     * @return Value of the Least Common Node.
+     * @param nodeValue The value of a binary tree node.
+     * @return The depth level of the input node.
      */
-    public int leastCommonNode() {
-        List<Node> common;
-        if (branch1.size() > branch2.size()) {
-            common = new ArrayList<>(branch2);
-            common.retainAll(branch1);
-        } else {
-            common = new ArrayList<>(branch2);
-            common.retainAll(branch1);
-        }
-        return common.stream().max(Comparator.comparing(Node::getValue))
-                .orElse(new Node(0)).getValue();
+    private int depth(int nodeValue) {
+        return (int) (Math.log10(nodeValue) / Math.log10(2));
+    }
+
+    /**
+     * Calculates the parent node value of the binary tree node.
+     *
+     * @param nodeValue The value of a binary tree node.
+     * @return The parent value of the binary tree node.
+     */
+    private int parent(int nodeValue) {
+        return nodeValue % 2 == 0 ? nodeValue / 2 : (nodeValue - 1) / 2;
     }
 }
--- chapter_004/src/test/java/ru/job4j/binarytree/BinaryTreeTest.java
+++ chapter_004/src/test/java/ru/job4j/binarytree/BinaryTreeTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.binarytree;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -13,31 +14,25 @@ import static org.junit.Assert.assertThat;
  * @since 16.05.2019
  */
 public class BinaryTreeTest {
+    private BinaryTree tree;
+
+    @Before
+    public void setUp() {
+        tree = new BinaryTree();
+    }
+
     @Test
     public void whenNodes9and5ThenCommon2() {
-        BinaryTree tree = new BinaryTree(9, 5);
-        int expected = 2;
-        assertThat(tree.leastCommonNode(), is(expected));
+        assertThat(tree.findParent(9, 5), is(2));
     }
 
     @Test
     public void whenNodes11and15ThenCommon1() {
-        BinaryTree tree = new BinaryTree(11, 15);
-        int expected = 1;
-        assertThat(tree.leastCommonNode(), is(expected));
+        assertThat(tree.findParent(11, 15), is(1));
     }
 
     @Test
     public void whenNodes13and15ThenCommon3() {
-        BinaryTree tree = new BinaryTree(13, 15);
-        int expected = 3;
-        assertThat(tree.leastCommonNode(), is(expected));
-    }
-
-    @Test
-    public void whenNodes13andMinus5ThenCommon0() {
-        BinaryTree tree = new BinaryTree(13, -5);
-        int expected = 0;
-        assertThat(tree.leastCommonNode(), is(expected));
+        assertThat(tree.findParent(13, 15), is(3));
     }
 }
