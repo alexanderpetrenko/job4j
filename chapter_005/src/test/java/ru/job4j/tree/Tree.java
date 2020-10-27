a161c006f6f1f3cbddf69a6cd0cb21a3bc6267d3
Oleksandr Petrenko <ext-petrenko@eurowag.com>
2020-10-27 10:05:52 +0100

    Tree class added

---
+++ chapter_005/src/test/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,9 @@
+package ru.job4j.tree;
+
+public class Tree<E> implements SimpleTree<E> {
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        return rsl;
+    }
+}
